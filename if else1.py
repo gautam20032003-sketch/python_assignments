dc7ea5b65d505e2e282a5a3c1aa0ce19b31d6025
gautam20032003-sketch <gautam20032003@gmail.com>
2025-12-08 11:40:00 +0530

    Create if else1.py

---
+++ if else1.py
@@ -0,0 +1,9 @@
+a=int(input('first number:'))
+b=int(input('second number:'))
+c=int(input('third number:'))
+if a>=b and a>= c:
+    print(a,'largest')
+elif b>=a and b>= c:
+    print(b,'largest')
+else:
+    print(c,'largest')      
