43bf06d9a5e70c41e297e0968fee562c9966cb31
gautam20032003-sketch <gautam20032003@gmail.com>
2025-12-08 11:41:32 +0530

    Create if else3.py

---
+++ if else3.py
@@ -0,0 +1,11 @@
+percentage=int(input("Enter percentage: "))
+if percentage >= 90:
+    print("A")
+elif percentage >= 80:
+    print("B")
+elif percentage >= 70:
+    print("C")
+elif percentage >= 60:
+    print("pass bss")
+else:
+    print("fail")
